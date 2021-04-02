f13c2f04ef80bacd985c5ff4b01ccf8365f11441
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2021-04-02 09:23:28 +0300

    Catch cleaned

--- tiralabra/build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"tiralabra"},{"l":"tiralabra.domain"}]
--- tiralabra/build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"tiralabra","l":"Main"},{"p":"tiralabra.domain","l":"Trie"},{"p":"tiralabra.domain","l":"TrieNode"}]
